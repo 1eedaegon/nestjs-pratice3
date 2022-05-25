43edcb4a3cf5bde208121f3ad9d177eadb852098
1eedaegon <d8726243@gmail.com>
2022-05-25 21:59:03 +0900

    Initialize project

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,70 @@
 import { Injectable } from '@nestjs/common';
+import { first } from 'rxjs';
+
+const { log } = console;
+// Make function decorator
+function firstDeco() {
+  console.log('first(): factory evaluated');
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    log('firstDeco(): called');
+  };
+}
+
+function secondDeco() {
+  log('second(): factory evaluated');
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    log('secondDeco() called');
+  };
+}
+
+class ExampleClass {
+  @firstDeco()
+  @secondDeco()
+  test() {
+    log('testing called');
+  }
+}
+
+const e = new ExampleClass();
+e.test();
+
+function thirdDeco<T extends { new (...args: any[]): any }>(constructor: T) {
+  return class extends constructor {
+    reportingURL = 'http://www.example.com';
+  };
+}
+
+@thirdDeco
+class BugReport {
+  type = 'report';
+  title: string;
+
+  constructor(t: string) {
+    this.title = t;
+  }
+}
+
+const bug = new BugReport('Needs dark mode');
+log(bug);
+// Method decorator practice
+function HandleError() {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    log(target, propertyKey, descriptor);
+    const method = descriptor.value;
+  };
+}
 
 @Injectable()
 export class AppService {
