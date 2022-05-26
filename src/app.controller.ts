6869b04b2522afaca132751936dbe9049f631ec2
1eedaegon <d8726243@gmail.com>
2022-05-26 23:07:22 +0900

    Warming up nest, custom header

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('/hel*')
+  getHello2(@Req() req: Request): string {
+    console.log(req);
     return this.appService.getHello();
   }
 }
---
+++ src/users/users.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  HttpCode,
+  BadRequestException,
+  Header,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll(@Res() res) {
+    const users = this.usersService.findAll();
+    return res.status(200).send(users);
+  }
+
+  @Header('Custom', 'Test Header')
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    if (+id > 1) throw new BadRequestException('id는 0보다 커야한다.');
+    return this.usersService.findOne(+id);
+  }
+
+  @HttpCode(202)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
