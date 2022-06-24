05271d44772097d17498e4b1a6f1bbcc64c5becf
1eedaegon <d8726243@gmail.com>
2022-06-24 19:15:14 +0900

    Add custom validation pipe

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateUserDto {
-  readonly name: string;
-  readonly email: string;
+  @IsString()
+  @MinLength(1)
+  @MaxLength(20)
+  name: string;
+
+  @IsEmail()
+  email: string;
+
   readonly password: string;
 }
