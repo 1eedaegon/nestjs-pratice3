cd0a99330f5d6d2e86cd7bfa2a83e6ea6eb8f5b8
1eedaegon <d8726243@gmail.com>
2022-07-20 22:53:17 +0900

    Update logging interceptor for http info using execution context

--- src/logging/logging.interceptor.ts
+++ src/logging/logging.interceptor.ts
@@ -1,21 +1,32 @@
 import {
   CallHandler,
   ExecutionContext,
+  Inject,
   Injectable,
+  Logger,
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable, tap } from 'rxjs';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
+  constructor(private readonly logger: Logger) {}
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    console.log('Before.');
+    const { method, url, body } = context.getArgByIndex(0);
+
+    this.logger.log(`Before this requests: ${method}: ${url}`);
     const now = Date.now();
     return next
       .handle()
-      .pipe(tap(() => console.log(`After ${Date.now() - now}ms.`)));
+      .pipe(
+        tap(() =>
+          this.logger.log(
+            `After this response: ${method}:${url} - ${Date.now() - now}ms.`,
+          ),
+        ),
+      );
   }
 }
--- src/logging/logging.module.ts
+++ src/logging/logging.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from './logging.interceptor';
 import { MyLogger2 } from './logging.service';
 
 @Module({
-  providers: [MyLogger2],
-  exports: [MyLogger2],
+  providers: [
+    MyLogger2,
+    Logger,
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+  ],
+  exports: [MyLogger2, Logger],
 })
 export class LoggingModule {}
